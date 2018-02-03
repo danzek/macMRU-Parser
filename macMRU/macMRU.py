ac5bf5516d426a3de635487c9fb5874385b0fd23
Dan <d@4n68r.com>
2018-02-03 11:25:14 -0600

    initial refactoring as package

--- macMRU/macMRU.py
+++ macMRU/macMRU.py
@@ -1,7 +1,9 @@
-#!/usr/bin/python
-'''
+#!/usr/bin/env python
+"""
 Copyright (c) 2017, Station X Labs, LLC
 All rights reserved.
+
+Contributions adding CSV output by Dan O'Day <d@4n68r.com>
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
@@ -24,7 +26,7 @@ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
 ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-'''
+"""
 
 import ccl_bplist
 from time import gmtime, strftime
